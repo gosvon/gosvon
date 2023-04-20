d7a63bf1f0edca5e2ad7a4e14f0d7c1529ab5f29
Gosvon <admin@gosvon.net>
2023-04-20 15:17:36 +0300

    script

---
+++ vk-metabot.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         GosVon Marking for VK
+// @namespace    vk-metabot-user-js
+// @description  Подсветка служебных страниц вконтакте.
+// @version      3.01
+// @homepageURL  https://vk.com/club187686148
+// @match        https://*.vk.com/*
+// @match        *://web.archive.org/*://*vk.com/*
+// @exclude      *://queuev4.vk.com/*
+// @connect      api.gosvon.net
+// @connect      script.gosvon.net
+// @updateURL    https://api.gosvon.net/update/vk-metabot.meta.js
+// @downloadURL  https://api.gosvon.net/update/vk-metabot.user.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM.xmlHttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @run-at       document-end
+// ==/UserScript==
